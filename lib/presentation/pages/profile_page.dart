e40ee0e5fe6b2097b0cb544c4141289eceb7de11
charderrasanjaya <charderrasanjaya@student.esaunggul.ac.id>
2024-10-17 14:04:21 +0000

    first commit

---
+++ lib/presentation/pages/profile_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'base_page.dart';
+import '../controllers/home_controller.dart';
+
+class ProfilePage extends StatelessWidget {
+  final HomeController controller;
+
+  ProfilePage(this.controller);
+
+  @override
+  Widget build(BuildContext context) {
+    return BasePage(
+      bodyContent: Profile(),
+      selectedIndex: 2, // Profile tab is selected
+      controller: controller,
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  // The URL of the image from the network
+  final String imageUrl =
+      'https://avatars.githubusercontent.com/u/110402853?v=4'; // Replace with your actual URL
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Display the profile image from the network
+            CircleAvatar(
+              radius: 80,
+              backgroundImage: NetworkImage(imageUrl),
+            ),
+            SizedBox(height: 20),
+            // User details
+            Text(
+              'Charderra Eka Bagas Sanjaya',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'sanjaya@gmail.com',
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.grey[600],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
